421fc8c54e9c8a852e843314fc87c1d4208a151e
Tirtho <thomas.tirole@ik.me>
2025-09-22 13:11:01 +0200

    [feat]: ajout de pages
    - faq
    - favoris
    - map

---
+++ src/pages/Favoris.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="mb-6 text-center">
+    Favoris
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/KantoMap.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="mb-6 text-center">
+    KantoMap
+  </h1>
+
+</template>
+
+<style scoped>
+
+</style>
